fa8b0d23e6f1880a47769b7d5072220a4089f50d
NguyenVuong1511 <nguyenvuong15112005@gmail.com>
2026-03-28 00:21:49 +0700

    Chạy thành công API Login, Register

--- BE/IdentityService/Models/RegisterRequest.cs
+++ BE/IdentityService/Models/RegisterRequest.cs
@@ -2,5 +2,9 @@
 {
     public class RegisterRequest
     {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        // Role sẽ được gán cứng là 'CANDIDATE' hoặc 'RECRUITER' tùy vào API họ gọi
+        public string FullName { get; set; } // Bắt buộc cho bảng Candidates
     }
 }
